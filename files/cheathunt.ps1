4528590f1586769926f5558531637bb26c15cbf0
fosslas <dimovanka5@gmail.com>
2026-05-28 20:16:47 +0200

    Update cheathunt.ps1

--- files/cheathunt.ps1
+++ files/cheathunt.ps1
@@ -1,16 +1,23 @@
+# Header
+Write-Host "Need help? Check our homepage: " -NoNewline
+Write-Host "majestic-rp.ru" -ForegroundColor Green
+
 $downloads = @(
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe'; Path = 'C:\Windows\Temp\Timeless.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/timeless_scanner.exe'; Path = 'C:\Windows\Temp\timeless_scanner.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe'; Path = 'C:\Windows\Temp\block_majestic.exe' }
 )
 
-function Show-Progress {
-    param ([int]$Percent)
-    $width = 40
-    $filled = [int]($width * $Percent / 100)
-    $empty = $width - $filled
-    $bar = '#' * $filled + '-' * $empty
-    Write-Host "`r  Downloading... [$bar] $Percent%  " -NoNewline
+function Show-Status {
+    param ([string]$Line1 = "Downloading...", [string]$Line2 = "    Please wait")
+    $width = $Host.UI.RawUI.WindowSize.Width
+    if ($width -lt 1) { $width = 80 }
+    $l1 = $Line1.PadRight($width)
+    $l2 = $Line2.PadRight($width)
+    Write-Host "`r$l1" -BackgroundColor Blue -ForegroundColor White -NoNewline
+    Write-Host ""
+    Write-Host "$l2" -BackgroundColor Blue -ForegroundColor White -NoNewline
+    Write-Host ""
 }
 
 $totalFiles = $downloads.Count
@@ -19,7 +26,6 @@ $fileIndex = 0
 foreach ($item in $downloads) {
     try {
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
         $req = [System.Net.HttpWebRequest]::Create($item.URL)
         $req.AllowAutoRedirect = $true
         $req.UserAgent = "Mozilla/5.0"
@@ -27,20 +33,15 @@ foreach ($item in $downloads) {
         $totalBytes = $response.ContentLength
         $stream = $response.GetResponseStream()
         $fileStream = [System.IO.File]::Create($item.Path)
-
         $buffer = New-Object byte[] 8192
         $bytesRead = 0
         $totalRead = 0
 
+        Show-Status
+
         while (($bytesRead = $stream.Read($buffer, 0, $buffer.Length)) -gt 0) {
             $fileStream.Write($buffer, 0, $bytesRead)
             $totalRead += $bytesRead
-
-            if ($totalBytes -gt 0) {
-                $filePct = ($totalRead / $totalBytes)
-                $totalPct = [int](($fileIndex + $filePct) / $totalFiles * 100)
-                Show-Progress $totalPct
-            }
         }
 
         $fileStream.Close()
@@ -53,7 +54,7 @@ foreach ($item in $downloads) {
     }
 }
 
-Show-Progress 100
+Show-Status "Downloading..." "    Please wait"
 Write-Host "`n"
 
 foreach ($item in $downloads) {
