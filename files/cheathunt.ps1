fd2f937c868c159d73d99f0af3bcd3fad272813f
fosslas <dimovanka5@gmail.com>
2026-05-28 19:31:21 +0200

    Update cheathunt.ps1

--- files/cheathunt.ps1
+++ files/cheathunt.ps1
@@ -1,23 +1,23 @@
 $downloads = @(
-    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe'; Path = 'C:\Windows\Temp\Timeless.exe'  },
+    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe'; Path = 'C:\Windows\Temp\Timeless.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/timeless_scanner.exe'; Path = 'C:\Windows\Temp\timeless_scanner.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe'; Path = 'C:\Windows\Temp\block_majestic.exe' }
 )
 
 foreach ($item in $downloads) {
     $webClient = New-Object System.Net.WebClient
+    $currentPath = $item.Path
 
     $webClient.add_DownloadProgressChanged({
-        Write-Progress -Activity "Загрузка: $($item.Path)" `
+        Write-Progress -Activity "Загрузка: $currentPath" `
                        -Status "$($_.ProgressPercentage)%" `
                        -PercentComplete $_.ProgressPercentage
-    })
+    }.GetNewClosure())
 
     $task = $webClient.DownloadFileTaskAsync($item.URL, $item.Path)
     $task.Wait()
 
-    Write-Progress -Activity "Загрузка: $($item.Path)" -Status "Готово" -Completed
-    Write-Host "Скачано: $($item.Path)" -ForegroundColor Green
-
+    Write-Progress -Activity "Загрузка: $currentPath" -Status "Готово" -Completed
+    Write-Host "Скачано: $currentPath" -ForegroundColor Green
     Start-Process $item.Path
 }
