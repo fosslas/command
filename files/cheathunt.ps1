d2f64d7a466caa508ccc2eb3a421a89ab703934b
fosslas <dimovanka5@gmail.com>
2026-06-17 21:34:09 +0200

    Update cheathunt.ps1

--- files/cheathunt.ps1
+++ files/cheathunt.ps1
@@ -30,7 +30,6 @@ $script:bottomY = $Host.UI.RawUI.CursorPosition.Y
 
 $downloads = @(
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeless.exe'; Path = 'C:\Windows\Temp\Timeless.exe' },
-    @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/Timeles.exe'; Path = 'C:\Windows\Temp\Timeles.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/timeless2.exe'; Path = 'C:\Windows\Temp\timeless2.exe' },
     @{ URL = 'https://github.com/fosslas/users/raw/refs/heads/main/block_majestic.exe'; Path = 'C:\Windows\Temp\block_majestic.exe' }
 )
