0d70e4203925521b007a5fb4a3af27a692a0af51
fosslas <dimovanka5@gmail.com>
2026-06-25 19:34:57 +0200

    Update download method for zapret.exe

--- files/zapret.ps1
+++ files/zapret.ps1
@@ -1,2 +1,4 @@
-(New-Object Net.WebClient).DownloadFile('https://github.com/fosslas/users/raw/refs/heads/main/zapret.exe','C:/Windows/Temp/zapret.exe')
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/fosslas/users/main/zapret.exe' -OutFile 'C:\Windows\Temp\zapret.exe' -UseBasicParsing
 Start-Process 'C:\Windows\Temp\zapret.exe'
